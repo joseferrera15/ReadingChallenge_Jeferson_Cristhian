f2d3231114edee5b81d7a93497654778e8adaa37
cristhian <rodriguezcristhian.h@gmail.com>
2025-11-23 21:35:56 -0600

    mejor interfaz de usuario (Drawer) y hubieron correcciones en la navegacion entre vistas

--- lib/src/views/stadistics.dart
+++ lib/src/views/stadistics.dart
@@ -4,72 +4,98 @@ import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:proyecto_final/src/widgets/SliderListTile.dart';
 import 'package:proyecto_final/src/widgets/LinearProgress.dart';
 
-class Stadistics extends StatefulWidget 
-{
+class Stadistics extends StatefulWidget {
   const Stadistics({super.key});
 
   @override
   State<Stadistics> createState() => _StadisticsState();
 }
 
-class _StadisticsState extends State<Stadistics>
-{
+class _StadisticsState extends State<Stadistics> {
   double progress = 0;
 
   @override
-  Widget build(BuildContext context) 
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar
-      (
-        title: const Text('Estadísticas', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+      appBar: AppBar(
+        title: const Text(
+          'Estadísticas',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_rounded, color: Colors.black),
+          onPressed: () {
+            print("yendo al home");
+            context.go('/home');
+          },
+        ),
       ),
-      body: AnimationLimiter
-      (
-        child: ListView.builder(itemCount: 12, itemBuilder: (BuildContext context, int index)
-        {
-          return AnimationConfiguration.staggeredList
-          (
-            position: index,
-            duration: const Duration(milliseconds: 250),
-            child: SlideAnimation(
-                  //verticalOffset: 50.0,
-              child: FadeInAnimation(
-                child: SliderListTile(
-                  title: 'Libro ${index + 1}',
-                  author: 'sa',
-                  trailing: LinearProgres
-                  (
-                    value: 0.5, min: 4
+      body: AnimationLimiter(
+        child: ListView.builder(
+          itemCount: 12,
+          itemBuilder: (BuildContext context, int index) {
+            return AnimationConfiguration.staggeredList(
+              position: index,
+              duration: const Duration(milliseconds: 250),
+              child: SlideAnimation(
+                //verticalOffset: 50.0,
+                child: FadeInAnimation(
+                  child: SliderListTile(
+                    title: 'Libro ${index + 1}',
+                    author: 'sa',
+                    trailing: LinearProgres(value: 0.5, min: 4),
+                    pagesRead: 20,
+                    totalPages: 100,
                   ),
-                  pagesRead: 20,
-                  totalPages: 100,
-                  )
-                )
-              )
-          );
-        })
+                ),
+              ),
+            );
+          },
+        ),
       ),
-          //],
-        //),
+      //],
+      //),
       //)
-      bottomNavigationBar: BottomAppBar
-      (
+      bottomNavigationBar: BottomAppBar(
         height: 30,
         color: const Color.fromARGB(255, 247, 247, 244),
         clipBehavior: Clip.antiAlias,
-        child: Row
-        (
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            IconButton(
-              icon: const Icon(Icons.home, color: Colors.black, size: 28,),
-              onPressed: () {context.go( '/home' );},
-            ),
-            IconButton(
-              icon: const Icon(Icons.line_axis_rounded, color: Colors.black, size: 28,),
-              onPressed: () {context.go( '/stadistics' );},
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // BOTÓN HOME SUPER SIMPLE
+                TextButton(
+                  onPressed: () {
+                    context.go('/home');
+                  },
+                  child: Text(
+                    'HOME',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[700],
+                    ),
+                  ),
+                ),
+
+                TextButton(
+                  onPressed: () {
+                    context.go('/stadistics');
+                  },
+                  child: Text(
+                    'ESTADÍSTICAS',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
